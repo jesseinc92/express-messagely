42fad5f673cf02bdeb8f3fc57b44d994c008e8d7
Jesse Hill <jesse.o.hill@gmail.com>
2021-12-10 19:50:57 -0500

    fixed the user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 /** User class for message.ly */
-
+const db = require('../db');
+const ExpressError = require('../expressError');
+const bcrypt = require('bcrypt');
+const { BCRYPT_WORK_FACTOR } = require('../config');
 
 
 /** User of the site. */
@@ -10,20 +13,56 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+    let hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const registrationTimestamp = new Date();
+
+    const result = await db.query(`
+      INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone, registrationTimestamp, registrationTimestamp]);
+
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    const result = await db.query(`
+      SELECT password FROM users
+      WHERE username=$1`,
+      [username]);
+
+    const user = result.rows[0];
+
+    if (user) {
+      return bcrypt.compare(password, user.password);
+    }
+    throw new ExpressError('Invalid username/password', 400);
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    const newTimestamp = new Date();
+
+    await db.query(`
+      UPDATE users SET last_login_at=$1
+      WHERE username=$2`,
+      [newTimestamp, username]);
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const results = await db.query(`
+      SELECT username, first_name, last_name, phone
+      FROM users`);
+
+    return results.rows;
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +73,15 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(`
+      SELECT username, first_name, last_name, phone, join_at, last_login_at
+      FROM users
+      WHERE username=$1`,
+      [username]);
+
+    return result.rows[0];
+  }
 
   /** Return messages from this user.
    *
@@ -44,7 +91,34 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const result = await db.query(
+      `SELECT m.id,
+              m.to_username,
+              u.first_name,
+              u.last_name,
+              u.phone,
+              m.body,
+              m.sent_at,
+              m.read_at
+        FROM messages AS m
+          JOIN users AS u ON m.to_username = u.username
+        WHERE from_username = $1`,
+      [username]);
+
+    return result.rows.map(m => ({
+      id: m.id,
+      to_user: {
+        username: m.to_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
+  }
 
   /** Return messages to this user.
    *
@@ -54,7 +128,28 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {
+    const results = await db.query(`
+    SELECT m.id, m.from_username, u.first_name, u.last_name, u.phone, m.body, m.sent_at, m.read_at
+    FROM users AS u
+    JOIN messages AS m
+    ON u.username = m.from_username
+    WHERE to_username=$1`,
+    [username]);
+
+    return results.rows.map(m => ({
+      id: m.id,
+      from_user: {
+        username: m.from_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
+  }
 }
 
 
